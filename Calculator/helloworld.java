3df328f6842c7d9fc859369a78ebabd112d90feb
Anshi Patel <anshipatel881@gmail.com>
2025-01-07 11:56:21 +0530

    my first commit

---
+++ Calculator/helloworld.java
@@ -0,0 +1,19 @@
+public class main{
+public static void main(String[] args){
+Scanner scn= new Scanner(System.in);
+int num1= scn.nextInt();
+int num2= scn.nextInt();
+
+System.out.println("Enter Operator");
+char operator = scr.next().charAt(0);
+double result;
+switch(operator){
+case'+':
+ result = num1+num2;
+Systrm.out.println(result);
+ break;
+default:
+Systrm.out.println(" Operator");
+
+}
+}
